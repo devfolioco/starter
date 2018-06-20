7f3248489769bd0687ab27c9c9b093d8578ac27f
ajayns <ajay.ns@outlook.com>
2018-06-20 14:11:51 +0530

    :recycle: Update dotenv example

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_ROOT = process.env.API_ROOT;
+const API_ROOT = 'https://jsonplaceholder.typicode.com/';
+console.log(process.env.API_ROOT);
 
 const requests = {
   get: (url, config) => axios.get(`${API_ROOT}${url}`, config),
