a99c7009db41a76c4434a78087ecd9096035bf4b
ajayns <ajay.ns@outlook.com>
2018-06-20 14:08:08 +0530

    :sparkles: webpack-dotenv

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_ROOT = 'https://jsonplaceholder.typicode.com/';
+const API_ROOT = process.env.API_ROOT;
 
 const requests = {
   get: (url, config) => axios.get(`${API_ROOT}${url}`, config),
