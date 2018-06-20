c1f48d338e6587b140d09ee48495a91604137289
ajayns <ajay.ns@outlook.com>
2018-06-20 13:26:58 +0530

    :sparkles: Add localStorage

--- src/store.js
+++ src/store.js
@@ -1,12 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { getItemFromStorage, storeItem } from './helpers/localStorage';
+
+// Get initial state from localStorage
+const persistedState = { todos: getItemFromStorage('todos') };
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(thunk)));
+
+// Persist state to localStorage
+store.subscribe(() => {
+  storeItem('todos', store.getState().todos);
+});
 
 export default store;
